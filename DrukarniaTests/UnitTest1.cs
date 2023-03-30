067f92cd739a4e4efac780a77cb8e81ff358029d
Glareforme <91750407+Glareforme@users.noreply.github.com>
2023-03-30 22:56:05 +0300

    Add project files.

---
+++ DrukarniaTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace DrukarniaTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
