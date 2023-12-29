d40f85e021eb50ce0c44144cdbae0a65a567f6b9
Glareforme <91750407+Glareforme@users.noreply.github.com>
2023-12-29 02:54:01 +0200

    add locators for post page

--- DrukarniaTests/POM/Locators/PostPageLoc.cs
+++ DrukarniaTests/POM/Locators/PostPageLoc.cs
@@ -18,5 +18,40 @@ namespace DrukarniaTests.POM.Locators
         public static By ArcticleTitleField = By.XPath("//h1[@class='article__title']");
         public static By ArcticleContentField = By.XPath("//h1[@class='article__title']/following-sibling::p");
         #endregion
+
+        #region Text modification
+        public static By HighlightBoldButton = By.XPath("//div//button[contains(@title, 'жирним')]");
+        public static By ItalicsButton = By.XPath("//div//button[contains(@title, 'Курсив')]");
+        public static By BigTitle = By.XPath("//div//button[contains(@title, 'Заголовок великий')]");
+        public static By SmallTitle = By.XPath("//div//button[contains(@title, 'Заголовок малий')]");
+        public static By LinkButton = By.XPath("//div//button[contains(@title, 'Посилання')]");
+        public static By BigQuoteButton = By.XPath("//div//button[contains(@title, 'Велика цитата')]");
+        public static By SmallQuoteButton = By.XPath("//div//button[contains(@title, 'Мала цитата')]");
+        public static By AddCapitalLetter = By.XPath("//div//button[contains(@title, 'Додати буквицю')]");
+        #endregion
+
+        #region Add element 
+        public static By AddElementButton = By.XPath("//div[@class='tippy-content']");
+        public static By AddPhotoToPageButton = By.XPath("//button[@title='Фото']");
+        public static By AddPhotoFromUnsplashButton = By.XPath("//button[contains(@title, 'Unsplash')] ");
+        public static By AddVideoButton = By.XPath("//button[contains(@title, 'Відео')] ");
+        public static By AddLinksButton = By.XPath("//button[contains(@title, 'Посилання на інші ресурси')]");
+        public static By SeparateButton = By.XPath("///button[@title='Розділити']");
+        public static By AddTableButton = By.XPath("//button[@title='Таблиця']");
+        public static By AddCodeButton = By.XPath("//button[@title='Код']");
+        #endregion
+
+        #region Table settings
+        public static By OpenTableSettingButton = By.XPath("//div[@id='plus-button']/button");
+        public static By AddRowToTableButton = By.XPath("//div[@id='table-toolbox']/button[@title='Додати рядок']");
+        public static By AddColumnToTableButton = By.XPath("//div[@id='table-toolbox']/button[@title='Додати стовпчик']");
+        public static By DeleteTableButton = By.XPath("//div[@id='table-toolbox']/button[@title='Видалити таблицю']");
+        #endregion
+
+        #region FAQ Button
+        public static By OpenFaqPopUp = By.Id("headlessui-menu-button-1");
+        public static By ListOfFrequentQuestions = By.Id("headlessui-menu-items-2");
+        #endregion
+
     }
 }
